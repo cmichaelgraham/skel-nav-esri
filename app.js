9b900db75109eb488f69b374fa8f3b1b7a50dba5
Mike Graham <mgraham@technicalmedia.com>
2015-01-07 04:34:13 -0700

    basic esri map is now loading

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
-var Greeter = (function () {
-    function Greeter(element) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-    Greeter.prototype.start = function () {
-        var _this = this;
-        this.timerToken = setInterval(function () { return _this.span.innerHTML = new Date().toUTCString(); }, 500);
-    };
-    Greeter.prototype.stop = function () {
-        clearTimeout(this.timerToken);
-    };
-    return Greeter;
-})();
-window.onload = function () {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+$(document).ready(function () {
+    require(["esri/map", "dojo/domReady!"], function (Map) {
+        var map = new Map("map", {
+            center: [-118, 34.5],
+            zoom: 8,
+            basemap: "topo"
+        });
+    });
+});
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,28 +1,9 @@
-﻿class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
-
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
-    }
-
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
-    }
-
-    stop() {
-        clearTimeout(this.timerToken);
-    }
-
-}
-
-window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
-};
+﻿$(document).ready(() =>
+{
+    require(["esri/map", "dojo/domReady!"], function (Map) {
+    var map = new Map("map", {
+        center: [-118, 34.5],
+        zoom: 8,
+        basemap: "topo"
+    });
+})});
