d19f590dcfaf3ebf4cbed132ac2137d4458ecac4
Mike Graham <mgraham@technicalmedia.com>
2015-01-09 15:54:04 -0700

    moving computers

--- typings/core-js/core-js.d.ts
+++ typings/core-js/core-js.d.ts
@@ -1,8 +1,2 @@
 ﻿declare module "core-js" {
 }
-
-interface PromiseCallback { (resolve: (param: any) => void, reject: (param: any) => void): void; }
-declare class Promise {
-    constructor(callback: PromiseCallback);
-    then: (arg: any) => Promise;
-}
